557c042396601c361b41036422d97cb7cb7f8307
Ricardo Martins <ricardo@zeplin.io>
2022-07-20 15:09:26 +0100

    Updated more settings

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,7 @@
 module.exports = {
   framework: '@storybook/react',
   stories: ['../src/stories'],
-  addons: ['@storybook/preset-create-react-app'],
+  addons: ['@storybook/preset-create-react-app', 'storybook-zeplin/register', '@storybook/addon-actions', '@storybook/addon-links'],
   core: {
     builder: 'webpack5'
   }
